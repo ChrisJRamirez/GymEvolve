c87de6473c9314ffb57451c3d2918b7f8c869a9a
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-02-21 14:54:07 -0500

    created share folder for types, linked selectedpages, removed prettier taildwind config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: [require("prettier-plugin-taildwindcss")],
+  // plugins: [require("prettier-plugin-taildwindcss")],
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,26 +1,44 @@
-import {useState} from "react";
-import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
 
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
   const flexBetween = "flex justify-between items-center";
   return (
     <nav>
-      <div
-      className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt="logo" />
-
             <div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>Our Classes</p>
-                <p>Contact Us</p>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
               </div>
               <div className={`${flexBetween} gap-8`}>
                 <p>Sign In</p>
@@ -31,7 +49,7 @@ const Navbar = (props: Props) => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
